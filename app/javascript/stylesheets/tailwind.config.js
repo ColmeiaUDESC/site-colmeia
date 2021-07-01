e00ec604adc8d9885f58ff2aaaa650af50165ace
João Dematte <jgg.dematte@gmail.com>
2021-07-01 12:35:53 -0300

    retirado bootstrap e adicionado tailwind

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,8 +1,10 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: { colors: { gray: colors.trueGray } },
   },
   variants: {
     extend: {},
