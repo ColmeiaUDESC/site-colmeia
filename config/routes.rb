629e0d20576c8c40d9013c91e074036e56b49e3c
Luciano Wayand de Abreu <lucianowayand@hotmail.com>
2021-07-04 07:31:10 -0300

    Adicionada documentação referente a novas páginas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Exemplo: get:'test', to:'test#index'
+  # Ler primeiro tópico do docs.md
+  
     root 'main#index'
 
 end
