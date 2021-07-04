281d77c2474372f65b560471ffb84d3b8dfec02e
Luciano Wayand de Abreu <lucianowayand@hotmail.com>
2021-07-04 08:22:54 -0300

    feita a adição da gem bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
+### O modelo user possui:
+
+#name:string
+#email:string
+#password_digest:string
+#situacao:string
+#data_de_inicio:date
+
+#password:string virtual
+#password_confirmation:string virtual
+
 class User < ApplicationRecord
+    has_secure_password 
+    # Esse metodo transforma password e password_confirmation em uma senha encriptada chamada password digest
+    # Deste modo não existe forma de acesso a senha diretamente e garantimos assim a segurança das informações do usuário
+
 end
