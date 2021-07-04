26632ccdbefa711adcda09a8a40b84b6b2bfefca
Luciano Wayand de Abreu <lucianowayand@hotmail.com>
2021-07-04 10:23:38 -0300

    Adição do formulário de cadastro

--- app/controllers/cadastro_controller.rb
+++ app/controllers/cadastro_controller.rb
@@ -1,4 +1,11 @@
 class CadastroController < ApplicationController
     def index
+        @user = User.new
+    end
+
+    def create
+        # Para acessar os parametros individualmente se usa params[:parametro]
+        @user = User.new(params[:user])
+        redirect_to :root
     end
 end
