f2b291fb399958165ca41acfe271ecdbf70a4762
Luciano Wayand de Abreu <lucianowayand@hotmail.com>
2021-07-04 10:31:37 -0300

    Criado sistema de cadastro de usuarios no postgres

--- app/controllers/cadastro_controller.rb
+++ app/controllers/cadastro_controller.rb
@@ -5,7 +5,15 @@ class CadastroController < ApplicationController
 
     def create
         # Para acessar os parametros individualmente se usa params[:parametro]
-        @user = User.new(params[:user])
-        redirect_to :root
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to root_path, notice: "Conta criada com sucesso"
+        else
+            render :new
+        end
+    end
+
+    def user_params
+        params.permit(:email,:password,:data_de_inicio,:name,:situacao)
     end
 end
