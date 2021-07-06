7ebb99e9a7632579bf856b282a83f147c53a644e
João Dematte <jgg.dematte@gmail.com>
2021-07-06 09:17:29 -0300

    adicionado login, navbar verifica user logado e pagina de membros dinâmica

---
+++ db/migrate/20210704111300_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false
+      t.string :password_digest
+      t.string :name, null: false
+      t.string :situacao
+      t.date :data_de_inicio
+
+      t.timestamps
+    end
+  end
+end
