f723696e9a5b42b79389d98dc83b29d103dd39ed
João Dematte <jgg.dematte@gmail.com>
2021-07-06 13:45:47 -0300

    nova página de cadastro preparada, mudança na página de login

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
   # Exemplo: get:'test', to:'test#index'
   # Ler primeiro tópico do docs.md
-    root 'main#index'
+  root 'main#index'
 
-    # CADASTRO
-    get 'cadastro', to: 'cadastro#new'
-    post 'cadastro' , to: 'cadastro#create'
+  # CADASTRO
+  get 'cadastro', to: 'cadastro#new'
+  post 'cadastro' , to: 'cadastro#create'
 
-    # LOGIN
-    get 'login', to: 'sessions#new'
-    post 'login' , to: 'sessions#create'
+  # LOGIN
+  get 'login', to: 'sessions#new'
+  post 'login' , to: 'sessions#create'
 
-    delete 'logout', to: 'sessions#destroy'
-  
-    get 'membros', to: 'membros#index'
+  delete 'logout', to: 'sessions#destroy'
 
-    get 'projetos', to: 'projetos#index'
+  get 'membros', to: 'membros#index'
+
+  get 'projetos', to: 'projetos#index'
 end
