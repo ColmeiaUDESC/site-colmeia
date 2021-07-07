711ce45e9ead758f69dc4379062e8bb5c4632c09
GabrielFJunkes <gabriel.f.junkes@gmail.com>
2021-07-07 11:34:29 -0300

    Trocado paths em string para path do rails

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,7 +29,7 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: "Post was successfully updated." }
+        format.html { redirect_to dashboard_posts_path, notice: "Post was successfully updated." }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit, status: :unprocessable_entity }
