8acb4923124c353dc27d4b89ada51e2a0ac7ff91
GabrielFJunkes <gabriel.f.junkes@gmail.com>
2021-07-19 13:34:43 -0300

    Adicionado if para usuário não se excluir

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,11 @@ class ApplicationController < ActionController::Base
   def set_current_user
     if session[:user_id]
       Current.user = User.find_by(id: session[:user_id])
+      # se o usuario nao existir mais desloga ele e manda pra tela principal
+      if !Current.user
+        session[:user_id] = nil
+        redirect_to root_path
+      end
     end
   end
 
