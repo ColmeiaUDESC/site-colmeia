13fcf6004ec8f2fd5970ef6a636b81d3b80e4ba2
Luciano <lucianowayand@hotmail.com>
2021-07-22 12:38:51 -0300

    edição da pagina

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_11_134811) do
+ActiveRecord::Schema.define(version: 2021_07_22_151717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(version: 2021_07_11_134811) do
     t.index ["user_id"], name: "index_comentarios_on_user_id"
   end
 
+  create_table "links", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "desc"
@@ -32,6 +37,11 @@ ActiveRecord::Schema.define(version: 2021_07_11_134811) do
     t.string "users_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts_tags", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "tag_id", null: false
   end
 
   create_table "tags", force: :cascade do |t|
