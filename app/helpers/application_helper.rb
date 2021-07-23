085e48d7937d09c7d4360419477b10b5a386a841
GabrielFJunkes <69057368+GabrielFJunkes@users.noreply.github.com>
2021-07-23 01:25:13 +0000

    Pagina tags (#96)
    * Adicionado if para usuário não se excluir
    
    * Adicionado pag Tags no dash
    
    * adicionado restricao de tags vazias e iguais
    
    * Adicionado unique no campo nome de tags
    
    * arrumado limite de número de tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,9 @@ module ApplicationHelper
     return 'flex w-full justify-between text-gray-800 font-bold' if request.path == path
     'flex w-full justify-between text-gray-700'
   end
+
+  def current_blog_class?(path)
+    return 'inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white rounded bg-gray-900 focus:shadow-outline focus:outline-none dark:hover:bg-gray-800' if request.path.include?path
+    'inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white rounded hover:bg-gray-900 focus:shadow-outline focus:outline-none dark:hover:bg-gray-800'
+  end
 end
--- app/javascript/packs/tags.js
+++ app/javascript/packs/tags.js
@@ -11,5 +11,19 @@ document.addEventListener("turbolinks:load", () => {
     }
 
     let form = document.getElementById("postForm");
-    form.addEventListener("submit", checkBoxToInput, false);
+    if (form){
+        form.addEventListener("submit", checkBoxToInput, false);
+    }
+    window.pesquisarTags = () => {
+        var tags = "";
+        var tagsCheckbox = document.querySelectorAll('.tagsCheck');   
+        for (var i = 0; i < tagsCheckbox.length; i++) {   
+            if (tagsCheckbox[i].checked){
+                tags += tagsCheckbox[i].value+= "e";
+            }   
+        }
+        var url = base_url = window.location.origin;
+        window.location.href = url+window.location.pathname.split('/p/')[0]+'/p/1/'+tags
+    }
+    
 })
