33d025f902a74d2197ca7876a3b4d87ed3d81aa8
GabrielFJunkes <gabriel.f.junkes@gmail.com>
2021-10-04 16:42:15 -0300

    Arruma prod dnv

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.smtp_settings = {
     :user_name => 'apikey',
-    :password => Api.last.key || '',
+    :password => 'Api.last.key',
     :domain => 'colmeiaudesc.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
