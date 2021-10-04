90ffe3030b12b77de96b548954c6b890c8d188a4
GabrielFJunkes <69057368+GabrielFJunkes@users.noreply.github.com>
2021-10-04 19:20:46 +0000

    Corrigir acesso da chave da api no prod (#128)
    * Adicionado esqueci senha e envio de email
    
    * Correção chave api email

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.smtp_settings = {
     :user_name => 'apikey',
-    :password => ENV['SENDGRID_API_KEY'],
+    :password => Api.last.key,
     :domain => 'colmeiaudesc.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_29_225735) do
+ActiveRecord::Schema.define(version: 2021_10_04_191504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "apis", force: :cascade do |t|
+    t.string "key"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "artefatos", force: :cascade do |t|
     t.string "texto"
